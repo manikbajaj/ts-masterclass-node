383ef32d83780c5e76ba8105ea17f21d4f653769
Manik <manik@pixelconception.com>
2025-02-20 17:32:38 +0530

    Final code for TCC404

--- src/config/container.config.ts
+++ src/config/container.config.ts
@@ -1,8 +1,11 @@
 import { Container } from "inversify";
 import { TasksController } from "../tasks/tasks.controller";
 import { TasksRouter } from "../tasks/tasks.router";
+import { UserController } from "../user/user.controller";
 
 export const container: Container = new Container();
-
+/* Tasks */
 container.bind(TasksController).toSelf().inTransientScope();
 container.bind(TasksRouter).toSelf().inTransientScope();
+/* Users */
+container.bind(UserController).toSelf().inTransientScope();
