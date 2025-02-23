6dda0786926290e79ecd803454a5752deebcb7e8
Manik <manik@pixelconception.com>
2025-02-23 14:16:53 +0530

    Final code for TCC463

--- index.ts
+++ index.ts
@@ -1,13 +1,21 @@
 import "reflect-metadata";
-
 import express, { Express, Request, Response } from "express";
-
 import { addRoutes } from "./src/config/routes.config";
 import mongoose from "mongoose";
 import { responseFormatter } from "./src/middleware/responseFormatter";
+import cors, { CorsOptions } from "cors";
+import * as dotenv from "dotenv";
 
 const app: Express = express();
 const port = 3001;
+
+let corsOptions: CorsOptions = {
+  origin: "http://example.com",
+};
+
+dotenv.config();
+
+app.use(cors());
 
 // Process Incoming request body
 app.use(express.json());
@@ -23,13 +31,15 @@ app.get("/", (req: Request, res: Response) => {
 addRoutes(app);
 
 async function bootstrap() {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("Cannot read environment variables");
+    process.exit(1);
+  }
+
   try {
-    await mongoose.connect(
-      "mongodb+srv://manik:pPWN0WSVYRg06BLS@nodejs.7rkt0.mongodb.net/",
-      {
-        dbName: "fullstackTasks",
-      }
-    );
+    await mongoose.connect(process.env.DATABASE_URL, {
+      dbName: process.env.DATABASE_NAME,
+    });
     console.log("Connnected To MongoDB");
     app.listen(port, () => {
       console.log(`App listening on port ${port}`);
